78acd61c0546b121158439515a75ebffc750d684
Faugnell <90843661+Faugnell@users.noreply.github.com>
2023-04-15 20:57:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cotton_ticket/StartActivity.kt
@@ -0,0 +1,17 @@
+package com.example.cotton_ticket
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cotton_ticket.ui.login.LoginFragment
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, LoginFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
