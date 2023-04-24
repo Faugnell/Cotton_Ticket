13f093ee49700fa66593fe3e5ce0e539019986dd
Faugnell <90843661+Faugnell@users.noreply.github.com>
2023-04-24 16:48:34 +0200

    fragment_home.xml IHM

--- app/src/main/java/com/example/cotton_ticket/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cotton_ticket/ui/home/HomeFragment.kt
@@ -12,6 +12,7 @@ import com.example.cotton_ticket.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    //private val navController = findNavController()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,7 +24,7 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this)[HomeViewModel::class.java]
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -32,7 +33,19 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.buttonTechnique.setOnClickListener { ajouterTechnique() }
+        binding.buttonPedagogique.setOnClickListener { ajouterPedagogique() }
+
         return root
+    }
+
+    private fun ajouterPedagogique() {
+        //navController.navigate(R.id.action_nav_home_to_nav_peda)
+    }
+
+    private fun ajouterTechnique() {
+        //navController.navigate(R.id.action_nav_home_to_nav_technique)
     }
 
     override fun onDestroyView() {
