7639e75b8827e5aa86c8029164a97ed53b941f95
Faugnell <90843661+Faugnell@users.noreply.github.com>
2023-04-24 22:55:48 +0200

    24/04/2023 : IHM liste ticket

--- app/src/main/java/com/example/cotton_ticket/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cotton_ticket/ui/home/HomeFragment.kt
@@ -7,12 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.cotton_ticket.R
 import com.example.cotton_ticket.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-    //private val navController = findNavController()
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -34,18 +36,10 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
 
-        binding.buttonTechnique.setOnClickListener { ajouterTechnique() }
-        binding.buttonPedagogique.setOnClickListener { ajouterPedagogique() }
+        binding.buttonTechnique.setOnClickListener { findNavController().navigate(R.id.action_nav_home_to_nav_technique) }
+        binding.buttonPedagogique.setOnClickListener { findNavController().navigate(R.id.action_nav_home_to_nav_peda) }
 
         return root
-    }
-
-    private fun ajouterPedagogique() {
-        //navController.navigate(R.id.action_nav_home_to_nav_peda)
-    }
-
-    private fun ajouterTechnique() {
-        //navController.navigate(R.id.action_nav_home_to_nav_technique)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/cotton_ticket/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/cotton_ticket/ui/list/ListViewModel.kt
@@ -3,5 +3,5 @@ package com.example.cotton_ticket.ui.list
 import androidx.lifecycle.ViewModel
 
 class ListViewModel : ViewModel() {
-
+    // TODO: Implement the ViewModel
 }
