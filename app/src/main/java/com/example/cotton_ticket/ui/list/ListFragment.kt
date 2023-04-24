c573bb52fbb1e3e242f41988e6b8927c611d9b09
Faugnell <90843661+Faugnell@users.noreply.github.com>
2023-04-24 22:24:49 +0200

    24/04/2023 : IHM liste ticket

--- app/src/main/java/com/example/cotton_ticket/ui/list/ListFragment.kt
+++ app/src/main/java/com/example/cotton_ticket/ui/list/ListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cotton_ticket.databinding.FragmentListBinding
@@ -27,11 +26,6 @@ class ListFragment : Fragment() {
 
         _binding = FragmentListBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textGallery
-        listViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
--- app/src/main/java/com/example/cotton_ticket/ui/list/ListViewModel.kt
+++ app/src/main/java/com/example/cotton_ticket/ui/list/ListViewModel.kt
@@ -1,13 +1,7 @@
 package com.example.cotton_ticket.ui.list
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ListViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "Ici s'affiche la liste de tout les tickets"
-    }
-    val text: LiveData<String> = _text
 }
